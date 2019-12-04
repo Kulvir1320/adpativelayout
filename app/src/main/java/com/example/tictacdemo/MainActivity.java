03af7f06c36080b58f8f9104fdedf75a7b70c3e4
Kulvir Dhillon <kulvirdhillon240@gmail.com>
2019-12-04 16:08:58 -0500

    game commit

---
+++ app/src/main/java/com/example/tictacdemo/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.tictacdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    /*
+    * 0 is a player yellow
+    * 1 is player red
+     */
+
+    private int player = 0;
+
+    /*
+    * initial state of the game
+     */
+    private int[] gamestate = {-1, -1,-1,-1,-1,-1,-1,-1};
+
+    public  void dropImage(View v){
+        ImageView imageView = (ImageView) v;
+        int state = Integer.valueOf(imageView.getTag().toString());
+        if (gamestate[state] == -1) {
+            gamestate[state] = player;
+
+            imageView.setTranslationY(-1500);
+            if (player == 0) {
+                imageView.setImageResource(R.drawable.yellow);
+                player = 1;
+            } else {
+                imageView.setImageResource(R.drawable.red);
+                player = 0;
+            }
+            imageView.animate().translationYBy(1500).setDuration(300);
+        }
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
